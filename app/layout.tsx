c9d1b24d1c5e75b4bedf6bd0395dedeefb613e81
Yauheni Novik <yauheni.novik@chdp-tech.net>
2023-07-06 18:38:44 +0200

    feat: finish project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,20 @@
-import './globals.css'
-import React from "react";
-import {Footer, Navbar} from "@/components";
+import "./globals.css";
+
+import { Footer, NavBar } from "@components";
 
 export const metadata = {
-  title: 'Car Hub',
-  description: 'Discover the best cars in the world.',
-}
+  title: "Car Hub",
+  description: "Discover world's best car showcase application",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className='relative'>
-            <Navbar />
-                 {children}
-            <Footer />
+        <NavBar />
+        {children}
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
